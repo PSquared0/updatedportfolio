cf94560f23db2233a6de92754e24d4792d7cca3f
Pascale L Pierre <Psquared@MacBook-Pro-2.local>
2023-07-15 16:50:10 -0500

    added img, downstream portfolio pages, updated h1 font

--- process.php
+++ process.php
@@ -1,84 +1,60 @@
-<?php
-if( isset($_POST) ){
+<?php error_reporting(0); ?>
+<html lang="en">
+  <head>
+    <title>Secure contact form</title>
+    <meta charset="utf-8">
+    <style>
+      p {
+        margin: 0;
+        color: red;
+      }
+    </style>
+  </head>
+  <body>
 
-    //form validation vars
-    $formok = true;
-    $errors = array();
+    <?php
+      if(isset($_POST['submit'])){
+        $name = htmlspecialchars(stripslashes(trim($_POST['name'])));
+        $subject = htmlspecialchars(stripslashes(trim($_POST['subject'])));
+        $email = htmlspecialchars(stripslashes(trim($_POST['email'])));
+        $message = htmlspecialchars(stripslashes(trim($_POST['message'])));
+        if(!preg_match("/^[A-Za-z .'-]+$/", $name)){
+          $name_error = 'Invalid name';
+        }
+        if(!preg_match("/^[A-Za-z .'-]+$/", $subject)){
+          $subject_error = 'Invalid subject';
+        }
+        if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)){
+          $email_error = 'Invalid email';
+        }
+        if(strlen($message) === 0){
+          $message_error = 'Your message should not be empty';
+        }
+      }
+    ?>
 
-    //submission data
-    $ipaddress = $_SERVER['REMOTE_ADDR'];
-    $date = date('d/m/Y');
-    $time = date('H:i:s');
-
-    //form data
-    $name = $_POST['fullName'];
-    $email = $_POST['email'];
-    $enquiry = $_POST['subject'];
-    $message = $_POST['message'];
-
-    //form validation to go here....
-    //validate name is not empty
-if(empty($name)){
-    $formok = false;
-    $errors[] = "You have not entered a name";
-}
-}
-
-//validate email address is not empty
-if(empty($email)){
-    $formok = false;
-    $errors[] = "You have not entered an email address";
-//validate email address is valid
-}elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    $formok = false;
-    $errors[] = "You have not entered a valid email address";
-}
-//validate message is not empty
-if(empty($message)){
-    $formok = false;
-    $errors[] = "You have not entered a message";
-}
-//validate message is greater than 20 characters
-elseif(strlen($message) < 20){
-    $formok = false;
-    $errors[] = "Your message must be greater than 20 characters";
-}
-
-//send email if all is ok
-if($formok){
-    $headers = "From: info@example.com" . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $emailbody = "<p>You have recieved a new message from the enquiries form on your website.</p>
-<p><strong>Name: </strong> {$name} </p>
-<p><strong>Email Address: </strong> {$email} </p>
-<p><strong>Telephone: </strong> {$telephone} </p>
-<p><strong>Enquiry: </strong> {$enquiry} </p>
-<p><strong>Message: </strong> {$message} </p>
-<p>This message was sent from the IP Address: {$ipaddress} on {$date} at {$time}</p>";
-
-    mail("pascalelpierre@gmail.com","New Enquiry",$emailbody,$headers);
-
-}
-//what we need to return back to our form
-$returndata = array(
-    'posted_form_data' => array(
-        'name' => $name,
-        'email' => $email,
-        'subject' => $enquiry,
-        'message' => $message
-    ),
-    'form_ok' => $formok,
-    'errors' => $errors
-);
-
-//if this is not an ajax request
-if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) !== 'xmlhttprequest'){
-
-    //set session variables
-    session_start();
-    $_SESSION['cf_returndata'] = $returndata;
-
-    //redirect back to form
-    header('location: ' . $_SERVER['HTTP_REFERER']);
-}
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+      <label for="name">Name:</label><br>
+      <input type="text" name="name">
+      <p><?php if(isset($name_error)) echo $name_error; ?></p>
+      <label for="subject">Subject:</label><br>
+      <input type="text" name="subject">
+      <p><?php if(isset($subject_error)) echo $subject_error; ?></p>
+      <label for="email">Email:</label><br>
+      <input type="text" name="email">
+      <p><?php if(isset($email_error)) echo $email_error; ?></p>
+      <label for="message">Message:</label><br>
+      <textarea name="message"></textarea>
+      <p><?php if(isset($message_error)) echo $message_error; ?></p>
+      <input type="submit" name="submit" value="Submit">
+      <?php
+        if(isset($_POST['submit']) && !isset($name_error) && !isset($subject_error) && !isset($email_error) && !isset($message_error)){
+          $to = 'pascalelpierre@gmail.com'; // edit here
+          $body = " Name: $name\n E-mail: $email\n Message:\n $message";
+          if(mail($to, $subject, $body)){
+            echo '<p style="color: green">Message sent</p>';
+          }else{
+            echo '<p>Error occurred, please try again later</p>';
+          }
+        }
+      ?>
